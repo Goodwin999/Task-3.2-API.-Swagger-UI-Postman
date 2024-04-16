c01b03e32248af6d0f6671bd9bc2ecbe346e436c
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2024-04-16 23:19:23 +0300

    first step completed

---
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -0,0 +1,62 @@
+package ru.hogwarts.school.model;
+
+import java.util.Objects;
+
+public class Faculty {
+
+    private Long id;
+    private String name;
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Faculty(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return age == faculty.age && Objects.equals(id, faculty.id) && Objects.equals(name, faculty.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
